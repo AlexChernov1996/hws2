bc098b45f6e37be95421aa13bf05b969d654dd36
Alexandr Chernov <vonrehcxela@gmail.com>
2023-12-29 16:39:47 +0200

    HW 10 has done

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import { restoreState, saveState } from './localStorage/localStorage'
 import s2 from '../../s1-main/App.module.css'
@@ -13,13 +13,14 @@ import s from './HW6.module.css'
 
 const HW6 = () => {
     const [value, setValue] = useState<string>('')
-
+useEffect(()=>{
+   setValue(restoreState('hw6-editable-span-value', value))
+},[])
     const save = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
-        // делают студенты
-
+       setValue(restoreState('hw6-editable-span-value',value))
     }
 
     return (
